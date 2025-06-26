fe3fc626d4350c14c4e8d5989a443d42d201ee84
Minrocha20 <yasmin.vitoria.rocha@escola.pr.gov.br>
2025-06-26 12:36:34 +0000

    adicionando script.js

---
+++ scriot.js
@@ -0,0 +1,7 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavraschave');
+
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+
+function mostraPalavrasChave() {
+    alert('fui clicado!');
+}
